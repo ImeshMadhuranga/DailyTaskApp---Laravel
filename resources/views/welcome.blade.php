db1f1142bc39da7ec23c2845a63df9ec50554cf9
Imesh <imesh@sanmark.lk>
2021-01-29 19:21:41 +0530

    Some Updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
         <!-- Styles -->
         <style>
@@ -66,21 +67,13 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+            
 
             <div class="content">
                 <div class="title m-b-md">
                     DailyTaskApp
                 </div>
+                <a href="/tasks" class="btn btn-danger">Enter Now</a>
             </div>
         </div>
     </body>
